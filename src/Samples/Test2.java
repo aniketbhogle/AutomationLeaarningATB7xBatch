bf1bff2215f91b92c3990a2c4724454a4a9da065
Aniket Bhogle <aniket.bhogle123@gmail.com>
2024-09-01 16:45:01 +0530

    done

--- src/Samples/Test2.java
+++ src/Samples/Test2.java
@@ -2,6 +2,19 @@ package Samples;
 
 public class Test2 {
     public static void main(String[] args) {
-        //Single Comment
+        // Single Comment - This code will not run or executed
+        //  2+2 ?
+
+        // Purpose Comment ?
+        // Instructions to Jr QA or Dev
+        // explain
+
+        /**
+         * Multiple Comment
+         * Documentation Purpose
+         *
+         *
+         *
+         */
     }
 }
